4765fba1b0976a86ecde6959c620a0991626875e
sengeng <814823991@qq.com>
2018-01-01 22:08:41 +0800

    完成博客详情页跳转

--- Workspace/blogproject/blog/models.py
+++ Workspace/blogproject/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -14,7 +15,8 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=100)
 
-
+    def __str__(self):
+        return self.name
 class Tag(models.Model):
     """
     标签 Tag 也比较简单，和 Category 一样。
@@ -22,6 +24,8 @@ class Tag(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     """
@@ -58,3 +62,7 @@ class Post(models.Model):
     # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User)
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
